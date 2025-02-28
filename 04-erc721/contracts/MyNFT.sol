763f267500fafd865337ed3f7739fafab04edb8a
gilbertpdrqz123 <gilbert.pdrqz@gmail.com>
2025-02-28 11:09:09 +0800

    add

--- 04-erc721/contracts/MyNFT.sol
+++ 04-erc721/contracts/MyNFT.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721URIStorage {
     uint256 tokenId;
 
-    constructor() ERC721("MyERC721Token", "MET") {}
+    constructor() ERC721("PUSSY", "KFY") {}
 
     function mint() external {
-        // _safeMint(msg.sender, tokenId);
-        // _setTokenURI(
-        //     tokenId,
-        //     "https://path/to/metadata-uri"
-        // );
-        // tokenId++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(
+           tokenId,
+           "https://path/to/metadata-uri"
+        );
+        tokenId++;
     }
 }
